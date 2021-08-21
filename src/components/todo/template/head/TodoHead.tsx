b03e2a62fdb36205cfc14430502b2d7d3ebbb452
kwak-bs <iam-bs@naver.com>
2021-08-22 01:11:33 +0900

    FEAT : #1 Display current time

--- src/components/todo/template/head/TodoHead.tsx
+++ src/components/todo/template/head/TodoHead.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import { getTimeProps } from "antd/lib/date-picker/generatePicker";
+import React,{useState, useEffect} from "react";
 import styled from "styled-components";
+import {days, months} from "utils/constants";
 
 const TodoHeadBlock = styled.div`
-  display: flex;
+  display: inline-block;
   justify-content: center;
   padding-top: 52px;
   padding-bottom: 24px;
   border-bottom: 3px solid #33bb77;
+`;
+const Wrap = styled.div`
+  display: flex;
+  place-content: center;
 `;
 
 const DateText = styled.div`
@@ -21,15 +27,39 @@ const DayText = styled.div`
   padding-top: 5px;
 `;
 
+const TimeText = styled.div`
+  font-size: 15px;
+  color: #105f777d;
+  padding-top: 10px;
+`;
+
 const TodoHead = () => {
   //@TODO 현재 시간을 표시해야합니다.
-  const dayString = "Tuesday";
-  const dateString = "July 20, 2021";
+  const [now, setNow] = useState(new Date());
+  const day : string = days[now.getDay()];
+  const month : string = months[now.getMonth()];
+  const date : number = now.getDate();
+  const year : number = now.getFullYear();
+  let hour : string = now.getHours().toString().length < 2 ? '0'+now.getHours().toString() : now.getHours().toString();
+  let minute : string = now.getMinutes().toString().length < 2 ? '0'+now.getMinutes().toString() : now.getMinutes().toString();
+  let seconds : string = now.getSeconds().toString().length < 2 ? '0'+now.getSeconds().toString() : now.getSeconds().toString();
 
+  useEffect(() => {
+    setTimeout(()=> getNow(), 1000)
+  }, [now])
+  const getNow = () => {
+    setNow(new Date());
+  }
+  
   return (
     <TodoHeadBlock>
-      <DayText>{dayString}</DayText>
-      <DateText>{dateString}</DateText>
+      <Wrap>
+        <DayText>{day}</DayText>
+        <DateText>{month} {date}, {year}</DateText>
+      </Wrap>
+      <Wrap>
+        <TimeText>{hour} : {minute} : {seconds}</TimeText>
+      </Wrap>
     </TodoHeadBlock>
   );
 };
---
+++ src/utils/constants.ts
@@ -0,0 +1,24 @@
+export const months = [   
+  'January',
+  'Febuary',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const days = [ 
+  'Sunday',
+  'Mondy',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
